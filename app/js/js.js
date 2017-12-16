bf3557cc93141aef75292c146ded3db00ecca6df
enmalafeev <malafeev.en@yandex.ru>
2017-12-16 23:08:28 +0300

    fixed layout

---
+++ app/js/js.js
@@ -0,0 +1,4 @@
+function range () {
+	var val = $(".range__range").val();
+	$(".range__range").css({'background':'-webkit-linear-gradient(left, #0eaaa6 0%, #0eaaa6 '+val+'%, #cccccc '+val+'%, #cccccc 100%) '})
+}
